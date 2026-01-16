794ac440b27f906dc1d3e658d471bb72922310cc
Proj245 <Proj245XD@gmail.com>
2026-01-15 16:15:12 -0800

    added ofsset from far

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autoAlign.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autoAlign.java
@@ -55,6 +55,9 @@ public class autoAlign extends CommandBase {
     @Override
     public void execute() {
         if (!alignOn) return;
+        if(drivebase.getPose().getY() < 75)
+            angleOffset = 5;
+        else angleOffset = 0;
 
         pid.setPID(kp, ki, kd);
 
@@ -65,7 +68,7 @@ public class autoAlign extends CommandBase {
                 && !result.getFiducialResults().isEmpty() && result.getFiducialResults().get(0).getFiducialId() == scoringGoal.getAprilTagId()) {
 
             llOffset = Math.toRadians(
-                    result.getFiducialResults().get(0).getTargetXDegrees()
+                    result.getFiducialResults().get(0).getTargetXDegrees() + angleOffset
             ) * 1;
             llTimer.reset();
 
